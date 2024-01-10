8961dd6d116a2565ac402c165d573e186599f64a
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2024-01-10 10:45:55 +0100

    bbbbbbbbbbbbbbbbbbbbbbb

--- IeIAPI/IeIAPI/ControladorCarga.cs
+++ IeIAPI/IeIAPI/ControladorCarga.cs
@@ -88,10 +88,10 @@ namespace IeIAPI
                     numeros[2] = 0; // Corregidos
                     Console.WriteLine("\n-------------------------------");
                     Console.WriteLine("Inicio de extraccion 1");
-                    //string csvUrl = "https://raw.githubusercontent.com/CamarillaGuanxi/ExtractorJSON/main/IeIAPI/IeIAPI/MUR2.json";
+                    string csvUrl = "https://raw.githubusercontent.com/CamarillaGuanxi/ExtractorJSON/main/IeIAPI/IeIAPI/MUR2.json";
 
-                    //string jsonFilePath = await client.GetStringAsync(csvUrl);
-                    string jsonFilePath = "./MUR2.json";
+                    string jsonFilePath = await client.GetStringAsync(csvUrl);
+                    //string jsonFilePath = "./MUR2.json";
                     string jsonData = System.IO.File.ReadAllText(jsonFilePath);
 
                     string json = Extractor3JSON.ExtractorJSON(numeros, jsonData);
