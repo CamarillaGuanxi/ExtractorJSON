b35b1387836e1f725210f693f85cdb33a5ff2957
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2024-01-10 09:43:39 +0100

    Deberia ir

--- IeIAPI/IeIAPI/ControladorCarga.cs
+++ IeIAPI/IeIAPI/ControladorCarga.cs
@@ -44,10 +44,10 @@ namespace IeIAPI
                         numeros[2] = 0; // Corregidos
                         Console.WriteLine("\n-------------------------------");
                         Console.WriteLine("Inicio de extraccion 1");
-                        //string csvUrl = "https://raw.githubusercontent.com/CamarillaGuanxi/IEIback/main/IeIAPI/IeIAPI/MUR.json";
+                        string csvUrl = "https://raw.githubusercontent.com/CamarillaGuanxi/IEIback/main/IeIAPI/IeIAPI/MUR.json";
 
-                       // string jsonFilePath = await client.GetStringAsync(csvUrl);
-                        string jsonFilePath = "./MUR.json";
+                        string jsonFilePath = await client.GetStringAsync(csvUrl);
+                        //string jsonFilePath = "./MUR.json";
                         string jsonData = System.IO.File.ReadAllText(jsonFilePath);
 
 
